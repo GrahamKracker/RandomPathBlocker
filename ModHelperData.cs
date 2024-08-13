854687351e21adf033d25518b1f258b7e928b00a
GrahamKracker <86011515+GrahamKracker@users.noreply.github.com>
2024-08-13 02:51:35 -0400

    Update ModHelperData.cs

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -7,5 +7,5 @@ public static class ModHelperData
     public const string Version = "1.0.0";
     public const string RepoOwner = "GrahamKracker";
     public const string RepoName = "RandomPathBlocker";
-    public const string WorksOnVersion = "38.3";
+    public const string WorksOnVersion = "44.1";
 }
